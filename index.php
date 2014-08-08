0fe9240fc02fdf6bf7178fed25d7021cd7026095
Mulia Arifandy Nasution <mul14.net@gmail.com>
2014-08-09 00:56:47 +0700

    First init

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PT. PP Properti</title>
+
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/text.css">
+  <link rel="stylesheet" href="css/960.css">
+
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet/less" type="text/css" href="styles.less" />
+
+</head>
+<body>
+  <div class="top">
+    <div class="container_12">
+      
+      <div class="nav-top">
+        <a href="#">Tuesday, 1 July 2014 </a> &bull;
+        <a href="#">Career</a> &bull;
+        <a href="#">Contact Us</a> &bull;
+        <a href="#">Properti Online</a> &bull;
+        <a href="#">Customer Statisfaction Survey</a>
+      </div>
+
+      <div class="clearfix">
+        
+        <div class="grid_6">
+          <img src="img/logo.png" alt="">
+        </div>
+
+        <div class="grid_6" style="padding: 25px 0; text-align: right;">
+          
+          <span class="lang">
+            <a href="#" class="active">ID</a>
+            <a href="#">EN</a>
+          </span>
+          
+          <span class="search">
+            <form action="#" method="GET">
+              <input type="search" placeholder="Search">
+            </form>
+          </span>
+
+        </div>
+
+      </div>
+
+
+      <div class="nav-main">
+        <a class="active" href="#">Home</a>
+        <a href="#">Profile</a>
+        <a href="#">Projects</a>
+        <a href="#">Investor Relations</a>
+        <a href="#">CSR</a>
+        <a href="#">News &amp; Release</a>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="slider">
+    <img src="img/slide.jpg" alt="">
+  </div>
+
+  <div class="content">
+
+    <div class="container_12 clearfix">
+      <div class="grid_4">
+        <div class="clearfix">
+          <div class="box">
+            <i class="fa fa-eye"></i> VISI MISI PT PP PROPERTI
+          </div>
+          <div class="box">
+            <i class="fa fa-eye"></i> CORPORATE SOCIAL RESPONSIBILITY
+          </div>
+          <div class="box">
+            <i class="fa fa-eye"></i> The Chair Of The Board  Of Directors Says
+          </div>
+        </div>
+      </div>
+      <div class="grid_4">
+        <img src="img/map.png" alt="">
+      </div>
+      <div class="box grid_4">
+        <img src="img/stock.png" alt="">
+      </div>
+      
+    </div>
+
+    <div class="featured">
+
+      <div class="container_12">
+        <div class="grid_2">GADING</div>
+        <div class="grid_2">GRAND</div>
+        <div class="grid_2">GRAND</div>
+        <div class="grid_2">KEMANG</div>
+        <div class="grid_2">IDJ</div>
+        <div class="grid_2">IDSAdas</div>
+      </div>
+      
+    </div>
+
+    <div class="bla">
+
+      <div class="container_16">
+        
+        <h3>Project Highlight</h3>
+
+        <div class="grid_6">
+          <div class="grid_3">
+            <img src="img/news.jpg" class="alpha" alt="">
+          </div>
+          <div class="grid_3">
+            
+            <time datetime="2014-06-02">July 02, 2014</time>
+
+            Tower Ke 2 Pavilion Permata 
+            Apartment Mulai Dipasarkan PP 
+            Properti di Surabaya
+          </div>
+          <div class="clear"></div>
+
+          <div class="grid_2">
+            <img src="img/news.jpg" class="alpha" alt="">
+          </div>
+          <div class="grid_3">
+            
+            <time datetime="2014-06-02">July 02, 2014</time>
+
+            Tower Ke 2 Pavilion Permata 
+            Apartment Mulai Dipasarkan PP 
+            Properti di Surabaya
+          </div>
+
+        </div>
+
+        <div class="clear"></div>
+
+      </div>
+
+      <h3>Latest News</h3>
+
+      <div class="grid_4">
+
+        <div class="grid_2">
+          <img src="img/newsmini.png" class="alpha" alt="">
+        </div>
+        <div class="grid_2">
+          
+          <time datetime="2014-06-02">July 02, 2014</time>
+
+          Tower Ke 2 Pavilion Permata 
+          Apartment Mulai Dipasarkan PP 
+          Properti di Surabaya
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="our-business">
+      
+      <div class="container_12">
+
+        <h3 class="grid_12">Our Business</h3>
+
+        <div class="grid_4">
+          <a href="#">
+            <img src="img/our-business.jpg" alt="">
+          </a>
+        </div>
+        <div class="grid_4">
+          <a href="#">
+            <img src="img/asd_18.jpg" alt="">
+          </a>
+        </div>
+        <div class="grid_4">
+          <a href="#">
+            <img src="img/asd_20.jpg" alt="">
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+
+  <footer>
+
+    <div class="container_12">
+      
+      <div class="grid_3">
+        <dl>
+          <dt>About Us</dt>
+          <dd><a href="#">Executive Team</a></dd>
+          <dd><a href="#">Board of Directors</a></dd>
+          <dd><a href="#">Press Releases</a></dd>
+          <dd><a href="#">Company History</a></dd>
+          <dd><a href="#">Coorporate Responsibility</a></dd>
+        </dl>
+      </div>
+
+      <div class="grid_3">
+        <dl>
+          <dt>Careers</dt>
+          <dd><a href="#">Working Here</a></dd>
+        </dl>
+      </div>
+
+      <div class="grid_3">
+        <dl>
+          <dt>Contact Us</dt>
+          <dd><a href="#">Sales</a></dd>
+          <dd><a href="#">Support &amp; Training</a></dd>
+          <dd><a href="#">Office Location</a></dd>
+          <dd><a href="#">Media Contacts</a></dd>
+        </dl>
+      </div>
+
+      <div class="grid_3">
+        <dl>
+          <dt>Socials</dt>
+          <dd><a href="#">Facebook</a></dd>
+          <dd><a href="#">Twitter</a></dd>
+          <dd><a href="#">LinkedIn</a></dd>
+          <dd><a href="#">Google+</a></dd>
+          <dd><a href="#">Youtube</a></dd>
+        </dl>
+      </div>
+
+    </div>
+
+    <div class="container_12">
+      <div class="mini-footer">
+        <a href="#">Feedback [+]</a> &bull;
+        <a href="#">Sitemap</a> &bull;
+        <a href="#">Copyright</a> &bull;
+        <a href="#">Privacy Statement</a> &bull;
+        <a href="#">Terms of Use</a> 
+      </div>
+    </div>
+
+  </footer>
+
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.3/less.min.js"></script>
+</body>
+</html>
